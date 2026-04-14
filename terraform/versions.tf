62f23017ff795477ba6ebbeebacea3216fa7cdb7
Brenden <brendenmaxwel.oakland.edu>
2026-04-14 16:50:38 -0400

    fix versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "s3" {
-    bucket = "fastcards-tf-state-4160"
-    key = "fastcards/terraform.tfstate"
-    region = "us-east-2"
+    bucket         = "fastcards-tf-state-4160"
+    key            = "fastcards/terraform.tfstate"
+    region         = "us-east-2"
     dynamodb_table = "fastcards-tf-locks"
-    encrypt = true
+    encrypt        = true
   }
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
