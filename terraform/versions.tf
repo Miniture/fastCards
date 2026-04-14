03417030ad9ea1e406b40e7d0b5244143604148a
Brenden <brendenmaxwel.oakland.edu>
2026-04-14 16:42:43 -0400

    add s3 backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket = "fastcards-tf-state-4160"
+    key = "fastcards/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "fastcards-tf-locks"
+    encrypt = true
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
